3d0bf2d5477773d8b9f6a153db687a622a504659
Bini <isabelbini.ferrer@gracia.lasalle.cat>
2025-04-06 23:46:16 +0200

    Traducciones

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
 # M04-UF1-PR01-isabelbiniferreranechina
- Pràctica de la pàgina web Totsegur
+ Página web que simula una compañía de seguros, Totsegur. 
+ 
+HTML
 
- ASSEGURATS
- 6.432
- OFICINES
- 90
- PROFESSIONALS
- 640
- SATISFACCIÓ CLIENT
- 8.7
+ En la página inicio muestra la información inicial, los servicios, información acerca de la compañía y sus contactos.
+
+ En la página nosotros muestra información más detallada de Totsegur, sus objetivos y sus orígenes.
+
+ En la página servicios muestra detalladamente lo que ofrece la compañía y qué planes hay en sus seguros.
+
+ En contacto muestra us teléfonos para poder contactar con la empresa.
+
+ En el área privada és un sitio en que el usuario incia sesión y puede declarar siniestros y hacer otras consultas.
+
+CSS
+
+El archivo main.css es usado para los estilos de la cabecera y el pie de página, y también el tipo de letra usado y su tamaño en los contenidos de las páginas.
+
+Los otros archivos (index.css, nosaltres.css, serveis.css, contacte.css y area.css) són para personalizar el estilo de cada página y que se vea atractiva.
+
+etc
+
+JAVASCRIPT
+Los archivos .js agregan la interacción del usuario en la página.
+
+TRADUCCIÓN
+La carpeta i18n contiene la traducción de la página web en castellano.
--- js/main.js
+++ js/main.js
@@ -14,13 +14,36 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-async function changeLanguage(language) {
-  const newURL = window.location.protocol + '//' + window.location.host + window.location.pathname + '?language=' + language;
-  window.history.replaceState({}, '', newURL);
-  const response = await fetch(`./i18n/${lang}.json`);
-}
-
-function toggleLanguageDropdown() {
+function idioma() {
   const dropdown = document.getElementById("language-options");
   dropdown.classList.toggle("hidden");
 }
+
+function canvi(idioma) {
+  idiomaActual(idioma);
+}
+
+async function idiomaActual(idioma) {
+  if (idioma === 'ca') {
+    location.reload();
+    return;
+  } else if (idioma === 'es') {
+    const response = await fetch(`/i18n/es.json`); 
+    const data = await response.json();
+    const paginaActual = window.location.pathname.split('/').pop().split('.')[0];
+    const navLinks = document.querySelectorAll(".header a");
+    const footer = document.querySelector("footer");
+
+    navLinks[0].textContent = data.header["1"];
+    navLinks[1].textContent = data.header["2"];
+    navLinks[2].textContent = data.header["3"];
+    navLinks[3].textContent = data.header["4"];
+            
+    footer.children[0].textContent = data.footer.email;
+    footer.children[1].textContent = data.footer.phone;
+    footer.children[2].textContent = data.footer.address;
+
+    translate(paginaActual);
+  }
+}
+
